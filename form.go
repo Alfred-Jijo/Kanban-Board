c9ef835dc9d7931349666f1d9a8af95341a45229
Alfred-Jijo <alfredjijo28@gmail.com>
2023-12-31 18:08:49 +0000

    reset to clear

--- form.go
+++ form.go
@@ -66,3 +66,8 @@ func (f *Form) CreateTask() tea.Msg {
 	task := NewTask(f.focused, f.title.Value(), f.description.Value())
 	return task
 }
+
+func (f *Form) Reset() {
+	f.title.Reset()
+	f.description.Reset()
+}
